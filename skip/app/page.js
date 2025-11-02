9657ec9c7ed174a58433cc4f34c948ceed492b52
ankitrajput9 <rajputankit9630@gmail.com>
2025-11-02 08:50:22 +0530

    update

---
+++ skip/app/page.js
@@ -0,0 +1,17 @@
+import { Skiper25 } from '@/components/ui/skiper-ui/skiper25'
+import { Skiper3 } from '@/components/ui/skiper-ui/skiper3'
+import { Skiper40 } from '@/components/ui/skiper-ui/skiper40'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className=' '>Hello Every one !</h1>
+      {/* <Skiper3 />
+      <Skiper25/> */}
+      <Skiper40/>
+    </div>
+  )
+}
+
+export default page
